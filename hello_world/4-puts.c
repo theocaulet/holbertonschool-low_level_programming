3101ac86ad043b099f842166bdd24c5f7f681c2c
théo caulet <caulet.theo@gmail.com>
2025-10-20 12:51:21 +0000

    fix: Write a C program that printsProgramming is like building a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,6 @@
-puts("Programming is like building a multilingual puzzle");
-return (0);
+#include <stdio.h>
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
