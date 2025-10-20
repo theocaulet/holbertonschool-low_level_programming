ae0a08e11eacee5acddc71d36b3f993a238dff65
théo caulet <caulet.theo@gmail.com>
2025-10-20 13:17:28 +0000

    fix: Write a C program with the function puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Point d'entrée du programme
+*
+* Return: Toujours 0 (succès)
+*/
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	(return 0);
+	return (0);
 }
+
