9480fa9c4c424ac2b7cb2658508f41436ca75320
théo caulet <caulet.theo@gmail.com>
2025-10-21 12:18:02 +0000

    feat: Complete the source code

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print whether the number stored in the variable n
+ * Return: always 0
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
