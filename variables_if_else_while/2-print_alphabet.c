4c67ac8c3e26c82404da98b833a5adbd4cfae682
théo caulet <caulet.theo@gmail.com>
2025-10-22 09:31:44 +0000

    feat: prints the alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+	putchar(alphabet);
+	}
+	putchar('\n');
+
+return (0);
+}
+
