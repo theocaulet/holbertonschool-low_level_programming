143b393fa15c4c665a67bdeed5223a9e20d36640
théo caulet <caulet.theo@gmail.com>
2025-10-22 09:40:37 +0000

    feat: prints the alphabet in lowercase, and then in uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase, and then in uppercase
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+	putchar(alphabet);
+	}
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+	putchar(ALPHABET);
+	}
+	putchar('\n');
+
+return (0);
+}
