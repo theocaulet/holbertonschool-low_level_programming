3808ff563402a7491b132d931af5e8de19b0161f
théo caulet <caulet.theo@gmail.com>
2025-10-23 09:35:33 +0000

    fix:  prints _putchar, followed by a new line

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Print  prints _putchar, followed by a new line
+ * main - Print _putchar, followed by a new line
  *
  * Return: always 0
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
