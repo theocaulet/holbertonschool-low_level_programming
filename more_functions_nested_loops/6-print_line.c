8eec182f98f80ca4df8a56c58e93befbebd6b2fe
theocaulet <caulet.theo@gmail.com>
2025-10-27 15:29:24 +0100

    feat: draws a straight line in the terminal

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal
+ * @n: the number of times the character _ should be printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 0; i <= 4; i++)
+	{
+	_putchar('_');
+	}
+	}
+	_putchar('\n');
+}
