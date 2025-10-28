e97cdb8027fefcb0c86e6e9a68f944558939abea
theocaulet <caulet.theo@gmail.com>
2025-10-28 12:30:18 +0100

    fix: Write a program that prints the numbers from 1 to 100

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers from 1 to 100
+ * main - Prints the numbers from 1 to 100 in FizzBuzz format
  * Return: always 0
  */
 int main(void)
@@ -9,18 +9,20 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-       	{
+	{
 	if (i % 3 == 0 && i % 5 == 0)
-	printf("FizzBuzz ");
+	printf("FizzBuzz");
 	else if (i % 3 == 0)
-	printf("Fizz ");
+	printf("Fizz");
 	else if (i % 5 == 0)
-	printf("Buzz ");
+	printf("Buzz");
 	else
-	printf("%d ", i);
+	printf("%d", i);
+
 	if (i != 100)
 	printf(" ");
 	}
 	printf("\n");
-	return (0);
+	return 0;
 }
+
