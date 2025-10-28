ab942c982e8e9288711dbb8ee21b7a7082ca2bc2
theocaulet <caulet.theo@gmail.com>
2025-10-28 15:23:37 +0100

    feat: Create a function that takes a pointer to an int

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value of an integer to 98
+ * @n: pointer to the integer to be updated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 void swap_int(int *a, int *b);
+void reset_to_98(int *n);
 
 #endif
