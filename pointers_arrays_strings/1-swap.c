7b9b96125849ea3eac936986255e7c408e0a0a84
theocaulet <caulet.theo@gmail.com>
2025-10-28 14:50:07 +0100

    feat: Create a function that swaps the values of two integers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: First integer
+ * @b: Second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
