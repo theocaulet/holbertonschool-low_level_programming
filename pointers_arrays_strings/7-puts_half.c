3fff70343f79dcb8e46722646c570d82afee1081
theocaulet <caulet.theo@gmail.com>
2025-10-30 11:11:03 +0100

    fix: Change the half of the string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,25 @@
 #include "main.h"
 
 /**
- * puts_half - prints half of a string, followed by a new line
- * @str: Pointer of the string
+ * puts_half - prints half of a string
+ * @str: Pointer of a string
  */
 void puts_half(char *str)
 {
-	int i;
+	int i, j, len;
 
-	for (i = 5; str[i] != '\0'; i++)
-	{
-	if (i % 1 == 0)
-	_putchar(str[i]);
-	}
+	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	len++;
+
+	if (len % 2 == 0)
+	i = len / 2;
+	else
+	i = (len + 1) / 2;
+
+	for (j = i; str[j] != '\0'; j++)
+	_putchar(str[j]);
+
 	_putchar('\n');
 }
+
