ad0f4895f952dfe70f1e4780459dd5a5d3c42b2c
theocaulet <caulet.theo@gmail.com>
2025-10-31 15:26:59 +0100

    feat: Create a function that encodes a string

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -2,24 +2,27 @@
 #include <stdio.h>
 
 /**
- * *leet -  encodes a string
- * Return: always 0
+ * leet - encodes a string into 1337
+ * @str: pointer to the string to encode
+ * Return: pointer to the encoded string
  */
-char *leet(char *)
+char *leet(char *str)
 {
 	int i, j;
-	char base;
-	char leet;
+	char base[] = "aAeEoOtTlL";
+	char leet[] = "4433007711";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	for (j = 0; base[j] != '\0'; j++)
 	{
-	if (str[i]; == base[j])
+	if (str[i] == base[j])
 	{
-	str[i] = leet[j]
+	str[i] = leet[j];
+	break;
 	}
 	}
 	}
-	return (0);
+	return (str);
 }
+
