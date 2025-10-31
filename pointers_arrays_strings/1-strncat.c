6433660dffda692ca6f5fd9693f8361a269a71f3
theocaulet <caulet.theo@gmail.com>
2025-10-31 10:01:37 +0100

    feat: Create a function that concatenates two strings

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates n bytes from src
+ * @dest: Pointer to destination strings
+ * @src: Pointer to source string
+ * @n: bytes from src
+ * Return: Pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+	i++;
+	}
+	while (src[j] != '\0')
+	{
+	dest[i] = src[j];
+	src[j] = n;
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
