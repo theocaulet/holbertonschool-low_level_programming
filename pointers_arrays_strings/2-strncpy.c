8fe4599c54f485efa2e930376a535d4fe0564313
theocaulet <caulet.theo@gmail.com>
2025-10-31 10:36:06 +0100

    feat: Create a function that copies a string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncpy - copies the string
+ * @dest: Pointer to copy
+ * @src: The copy pointer
+ * @n: bytes from src
+ * Return: Pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *temp = dest;
+	int i = 0, j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	dest++;
+	src++;
+	}
+	*dest = '\0';
+	return (temp);
+}
