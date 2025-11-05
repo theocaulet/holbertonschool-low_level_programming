0cd76c732c9e9a37b3f077b1600d8d234fa1cd78
theocaulet <caulet.theo@gmail.com>
2025-11-05 15:07:57 +0100

    fix: Change recursive function

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,22 +1,36 @@
 #include "main.h"
 
 /**
- * is_prime_number - returns 1 if the integer is a prime number, otherwise 0
- * @n: The integer of the function
- * Return: 1 if the integer is a prime number, otherwise 0
+ * is_prime_helper - checks recursively if n is divisible bt i
+ * @n: number to check
+ * @i: current divisor
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_helper(int n, int i)
+{
+	if (n <= 0)
+	{
+	return (0);
+	}
+	if (i * i > n)
+	{
+	return (1);
+	}
+	if (n % i == 0)
+	{
+	return (0);
+	}
+	return (is_prime_helper(n, i + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if n is prime, 0 otherwise
+ * @n: number to check
+ *
+ * Return: 1 if prime, 0 if not
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-	{
-	return (0);
-	}
-	for (i = 2; i * i <= n; i++)
-	{
-	if (n % i == 0)
-	return (0);
-	}
-	return (1);
+	return (is_prime_helper(n, 2));
 }
+
