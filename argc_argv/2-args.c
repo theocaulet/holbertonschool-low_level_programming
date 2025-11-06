74876701addc460daf0afaf94e40d7a57e8afca3
theocaulet <caulet.theo@gmail.com>
2025-11-06 15:34:34 +0100

    feat: Create a function that prints all arguments it receives

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Description: prints all arguments it receives
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
