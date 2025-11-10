08dfdc9907dfdeeacdb161737b8ffd18d3f478ec
theocaulet <caulet.theo@gmail.com>
2025-11-10 15:34:48 +0100

    fix: Modify the function if

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,9 +14,12 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len1 = 0, len2 = 0;
 	char *array;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
 	s1 = "";
+	}
+	if (s2 == NULL)
+	{
 	s2 = "";
 	}
 	while (s1[len1] != '\0')
