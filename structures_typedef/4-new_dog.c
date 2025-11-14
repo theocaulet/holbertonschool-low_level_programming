ec151b5933114fe9e0a7a24f8ac04de2447bd855
theocaulet <caulet.theo@gmail.com>
2025-11-14 10:58:13 +0100

    fix: Change a function that creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,53 +1,46 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * new_dog - creates a new dog
+ * new_dog - Creates a new dog
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Name of the dog's owner
- * Return: The pointer to the new dog, or NULL on failure
+ *
+ * Return: Pointer to the new dog, or NULL on failure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int i, len;
+	int i, len_name = 0, len_owner = 0;
+	char *n, *o;
 
-	if (name == NULL)
-	{
-	return (NULL);
-	}
-	if (owner == NULL)
-	{
-	return (NULL);
-	}
+	if (!name)
+	name = "";
+	if (!owner)
+	owner = "";
+	while (name[len_name])
+	len_name++;
+	while (owner[len_owner])
+	len_owner++;
 	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-	{
+	if (!dog)
 	return (NULL);
-	}
-	for (len = 0; name[len]; len++)
-	dog->name = malloc(len + 1);
-	if (dog->name == NULL)
+	n = malloc(len_name + 1);
+	o = malloc(len_owner + 1);
+	if (!n || !o)
 	{
+	free(n);
+	free(o);
 	free(dog);
 	return (NULL);
 	}
-	for (i = 0; i <= len; i++)
-	dog->name[i] = name[i];
-	for (len = 0; owner[len]; len++)
-	dog->owner = malloc(len + 1);
-	if (dog->owner == NULL)
-	{
-	free(dog->name);
-	free(dog);
-	return (NULL);
-	}
-	for (i = 0; i <= len; i++)
-	dog->owner[i] = owner[i];
+	for (i = 0; i <= len_name; i++)
+	n[i] = name[i];
+	for (i = 0; i <= len_owner; i++)
+	o[i] = owner[i];
+	dog->name = n;
+	dog->owner = o;
 	dog->age = age;
 	return (dog);
 }
-
-
