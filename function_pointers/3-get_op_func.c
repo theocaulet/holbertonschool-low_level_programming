96b0004e160f77ec8b91a4e74a7e7318d84eaee9
theocaulet <caulet.theo@gmail.com>
2025-11-17 17:55:51 +0100

    feat: Create a program that performs simple operations

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * get_op_func - return a pointer to the function
+ * @s: The operator passed as argument to the program
+ * Return: A pointer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] =
+
+	{
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+	if (strcmp(s, ops[i].op) == 0)
+	{
+	return (ops[i].f);
+	}
+	i++;
+	}
+	return (NULL);
+}
+
+
