6970c1476f566c0d144237d4a7e2c8856e98bd15
theocaulet <caulet.theo@gmail.com>
2025-11-17 18:00:26 +0100

    fix: Modify file main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -33,6 +33,11 @@ int main(int argc, char *argv[])
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+	printf("Error\n");
+	return (100);
+	}
 	printf("%d\n", f(a, b));
 	return (0);
 }
