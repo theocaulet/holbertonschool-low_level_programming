05ddbe8834ef3109cde4a3b96418f79c4d629d56
theocaulet <caulet.theo@gmail.com>
2025-11-18 17:41:42 +0100

    fix: Modify emplacement of va_list

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,18 +11,19 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int j = 0;
+	va_list args;
 
 	if (n == 0)
 	{
 	return (0);
 	}
 
-	va_list args;
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 	int value = va_arg(args, int);
+
 	j += value;
 	}
 	va_end(args);
