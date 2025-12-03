d0986a7747bbfeecdaaae33e555da5a42d52253e
theocaulet <caulet.theo@gmail.com>
2025-12-03 13:20:03 +0100

    fix: Correct variable usage in sum_dlistint function for clarity

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -7,15 +7,14 @@
 */
 int sum_dlistint(dlistint_t *head)
 {
-	dlistint_t *current;
 	int sum = 0;
 
 	if (head == NULL)
 	return (0);
-	while (current != NULL)
+	while (head != NULL)
 	{
-	sum += current->n;
-	current = current->next;
+	sum += head->n;
+	head = head->next;
 	}
 	return (sum);
 }
